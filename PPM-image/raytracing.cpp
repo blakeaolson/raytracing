982da1711969d808983bd8f4b311bd8251085cca
blakeaolson <blakeaolson1@gmail.com>
2023-11-30 12:27:05 -0600

    small refactor

--- PPM-image/raytracing.cpp
+++ PPM-image/raytracing.cpp
@@ -7,6 +7,7 @@ int main() {
 
   std::cout << "P3\n" << image_width << " " << image_height << "\n255\n"; 
   for (int i = 0; i < image_height; ++i){
+    std::clog << "\rScanlines remaining: " << (image_height - i) << " " << std::flush;
     for (int j = 0; j < image_width; ++j){
       int r = i;
       int g = j;
