85a983b7121a1085c0004f3fcc22f769b0990902
blakeaolson <blakeaolson1@gmail.com>
2023-11-30 12:16:35 -0600

    initial commit

---
+++ raytracing.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+  int image_width = 256;
+  int image_height = 256;
+
+  std::cout << "P3\n" << image_width << " " << image_height << "\n255\n"; 
+  for (int i = 0; i < image_height; ++i){
+    for (int j = 0; j < image_width; ++j){
+      int r = i;
+      int g = j;
+      int b = 0;
+
+      std::cout << r << " " << g << " " << b << "\n";
+    }
+  }
+}
